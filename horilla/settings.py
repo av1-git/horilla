30dae0568c1d27fc8199fdca0c10fb906a0e4ce1
av1-git <av1@rjpi.co.in>
2026-03-13 19:55:25 +0530

    Update settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -122,21 +122,15 @@ if env("DATABASE_URL", default=None):
     }
 else:
     DATABASES = {
-        "default": {
-            "ENGINE": env("DB_ENGINE", default="django.db.backends.sqlite3"),
-            "NAME": env(
-                "DB_NAME",
-                default=os.path.join(
-                    BASE_DIR,
-                    "TestDB_Horilla.sqlite3",
-                ),
-            ),
-            "USER": env("DB_USER", default=""),
-            "PASSWORD": env("DB_PASSWORD", default=""),
-            "HOST": env("DB_HOST", default=""),
-            "PORT": env("DB_PORT", default=""),
-        }
-    }
+ 'default': {
+  'ENGINE': 'django.db.backends.postgresql',
+  'NAME': 'horillahrms',
+  'USER': 'horillaadmin',
+  'PASSWORD': 'HoAd@123',
+  'HOST': 'server.postgres.database.azure.com',
+  'PORT': '5432',
+ }
+}
 
 # DATABASES = {
 #     'default': {
